5f163e547800a39f7a9c8d6bfb9375e26e5d3114
张汉发 <73461997+zhanghanf@users.noreply.github.com>
2025-12-18 20:01:30 +0800

    heap
    heap测试

---
+++ moden-C-Learn/Chapter7/Heap.h
@@ -0,0 +1,43 @@
+#pragma once
+//控制自由存储区heap和bucket的分配和释放
+struct Bucket {
+	const static size_t SIZE = 4096;
+	std::byte data[SIZE];
+};
+
+struct Heap {
+	void* allocate(size_t size) {
+		if (size > Bucket::SIZE) {//请求的大小超过单个Bucket的大小
+			throw std::bad_alloc();
+		}
+		for (auto& bucket : buckets) {//查找一个未使用的Bucket
+			if (!bucketInUse[&bucket - buckets]) {
+				bucketInUse[&bucket - buckets] = true;
+				return static_cast<void*>(bucket.data);//返回Bucket的数据指针
+			}
+		}
+		throw std::bad_alloc();
+	}
+	void free(void* ptr) {//接受一个指针并释放对应的Bucket
+		for (auto& bucket : buckets) {
+			if (static_cast<void*>(bucket.data) == ptr) {
+				bucketInUse[&bucket - buckets] = false;
+				return;
+			}
+		}
+	}
+	static const size_t MAX_BUCKETS = 10;
+	Bucket buckets[MAX_BUCKETS]{};//包含MAX_BUCKETS个Bucket对象
+	bool bucketInUse[MAX_BUCKETS] = { false };//跟踪每个Bucket是否被使用
+};
+
+// 测试Heap和Bucket
+	//通过重载运算符new和delete来使用自定义的Heap分配器
+Heap heap;
+void* operator new(std::size_t size) {
+	return heap.allocate(size);
+}
+void operator delete(void* ptr) noexcept {
+	heap.free(ptr);
+}
+//布置运算符new和delete的重载
--- moden-C-Learn/Chapter7/main.cpp
+++ moden-C-Learn/Chapter7/main.cpp
@@ -1,6 +1,7 @@
 //hello world
 #include <iostream>
 #include <stdexcept>
+#include"Heap.h"
 //一个运行时溢出检查的Checked-Integer类
 struct CheckedInt {
 	int value;
@@ -38,49 +39,16 @@ struct CheckedInt {
 	}
 };
 
-//控制自由存储区heap和bucket的分配和释放
-struct Bucket {
-	const static size_t SIZE = 4096;
-	std::byte data[SIZE];
-};
-
-struct Heap {
-	void* allocate(size_t size) {
-		if (size > Bucket::SIZE) {//请求的大小超过单个Bucket的大小
-			throw std::bad_alloc();
-		}
-		for (auto& bucket : buckets) {//查找一个未使用的Bucket
-			if (!bucketInUse[&bucket - buckets]) {
-				bucketInUse[&bucket - buckets] = true;
-				return static_cast<void*>(bucket.data);//返回Bucket的数据指针
-			}
-		}
-		throw std::bad_alloc();
+struct Point {
+	int x;
+	int y;
+	Point(int x, int y) : x(x), y(y) {
+		printf("Point created%p\n",this);
 	}
-	void free(void* ptr) {//接受一个指针并释放对应的Bucket
-		for (auto& bucket : buckets) {
-			if (static_cast<void*>(bucket.data) == ptr) {
-				bucketInUse[&bucket - buckets] = false;
-				return;
-			}
-		}
+	~Point() {
+		printf("Point destroyed%p\n",this);
 	}
-	static const size_t MAX_BUCKETS = 10;
-	Bucket buckets[MAX_BUCKETS]{};//包含MAX_BUCKETS个Bucket对象
-	bool bucketInUse[ MAX_BUCKETS ] = { false };//跟踪每个Bucket是否被使用
 };
-
-// 测试Heap和Bucket
-	//通过重载运算符new和delete来使用自定义的Heap分配器
-Heap heap;
-void* operator new(std::size_t size) {
-	return heap.allocate(size);
-}
-void operator delete(void* ptr) noexcept {
-	heap.free(ptr);
-}
-//布置运算符new和delete的重载
-
 int main() {
 	CheckedInt a(INT_MAX);
 	CheckedInt b(3000);
