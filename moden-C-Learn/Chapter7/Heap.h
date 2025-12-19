ce4050f05dab9c5213252608ebff2bcc25074c46
张汉发 <73461997+zhanghanf@users.noreply.github.com>
2025-12-19 19:05:37 +0800

    创建LargeBucket类，最多存储1MB数据，扩展Heap类

--- moden-C-Learn/Chapter7/Heap.h
+++ moden-C-Learn/Chapter7/Heap.h
@@ -4,10 +4,21 @@ struct Bucket {
 	const static size_t SIZE = 4096;
 	std::byte data[SIZE];
 };
-
+//创建一个1MB的大桶
+struct LargeBucket {
+	const static size_t SIZE = 1024 * 1024;
+	std::byte data[SIZE];
+};
+//扩展heap类以支持大桶分配
 struct Heap {
 	void* allocate(size_t size) {
 		if (size > Bucket::SIZE) {//请求的大小超过单个Bucket的大小
+			for (auto& largeBucket : largeBuckets) {//查找一个未使用的大桶
+				if (!bucketInUse[&largeBucket - largeBuckets + MAX_BUCKETS]) {
+					bucketInUse[&largeBucket - largeBuckets + MAX_BUCKETS] = true;
+					return static_cast<void*>(largeBucket.data);//返回大桶的数据指针
+				}
+			}
 			throw std::bad_alloc();
 		}
 		for (auto& bucket : buckets) {//查找一个未使用的Bucket
@@ -25,10 +36,18 @@ struct Heap {
 				return;
 			}
 		}
+		for (auto& largeBucket : largeBuckets) {
+			if (static_cast<void*>(largeBucket.data) == ptr) {
+				bucketInUse[&largeBucket - largeBuckets + MAX_BUCKETS] = false;
+				return;
+			}
+		}
 	}
 	static const size_t MAX_BUCKETS = 10;
 	Bucket buckets[MAX_BUCKETS]{};//包含MAX_BUCKETS个Bucket对象
 	bool bucketInUse[MAX_BUCKETS] = { false };//跟踪每个Bucket是否被使用
+	LargeBucket largeBuckets[MAX_BUCKETS];//包含MAX_BUCKETS个LargeBucket对象
+	bool largeBucketInUse[MAX_BUCKETS] = { false };//跟踪每个LargeBucket是否被使用
 };
 
 // 测试Heap和Bucket
