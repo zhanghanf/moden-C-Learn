f2baa420b178ad14f7e2b4d050db5d8857bb8977
张汉发 <73461997+zhanghanf@users.noreply.github.com>
2026-01-09 15:52:01 +0800

    栈、队列、优先级队列学习

--- moden-C-Learn/Chapter13/Vector.cpp
+++ moden-C-Learn/Chapter13/Vector.cpp
@@ -150,6 +150,156 @@ BOOST_AUTO_TEST_CASE(ListBasicTest) {
 	++it;
 	BOOST_CHECK_EQUAL(*it, 55);
 	//merge合并两个链表
-	//lst.merge(mlst2, [](int x, int y) {return x <= y; });
+	lst.sort([](int a, int b) {return a >= b; });
+	mlst2.sort([](int a, int b) {return a >= b; });
+	lst.merge(mlst2, [](int a, int b) {return a > b; });
 }
 BOOST_AUTO_TEST_SUITE_END()
+//栈:针对定制情况公开了有限的接口
+#include<stack>
+BOOST_AUTO_TEST_SUITE(StackTest)
+BOOST_AUTO_TEST_CASE(StackBasicTest) {
+	std::stack<int> stk;
+	BOOST_CHECK(stk.empty());
+	stk.push(10);
+	stk.push(20);
+	BOOST_CHECK_EQUAL(stk.size(), 2);
+	BOOST_CHECK_EQUAL(stk.top(), 20);
+	stk.pop();
+	BOOST_CHECK_EQUAL(stk.size(), 1);
+	BOOST_CHECK_EQUAL(stk.top(), 10);
+	stk.pop();
+	BOOST_CHECK(stk.empty());
+}
+//使用栈包装向量
+BOOST_AUTO_TEST_CASE(StackVectorTest) {
+	std::stack<int, std::vector<int>> stk_vec;
+	BOOST_CHECK(stk_vec.empty());
+	stk_vec.push(100);
+	stk_vec.push(200);
+	BOOST_CHECK_EQUAL(stk_vec.size(), 2);
+	BOOST_CHECK_EQUAL(stk_vec.top(), 200);
+	stk_vec.pop();
+	BOOST_CHECK_EQUAL(stk_vec.size(), 1);
+	BOOST_CHECK_EQUAL(stk_vec.top(), 100);
+	stk_vec.pop();
+	BOOST_CHECK(stk_vec.empty());
+	//emplace测试
+	stk_vec.emplace(300);
+	BOOST_CHECK_EQUAL(stk_vec.top(), 300);
+	auto v2{ stk_vec };
+	BOOST_CHECK_EQUAL(v2.top(), 300);
+	stk_vec.push(400);
+	v2.swap(stk_vec);
+	BOOST_CHECK_EQUAL(v2.top(), 400);
+}
+BOOST_AUTO_TEST_SUITE_END()
+//队列:队列是一种先进先出（FIFO）的数据结构
+#include<queue>
+BOOST_AUTO_TEST_SUITE(QueueTest)
+BOOST_AUTO_TEST_CASE(QueueBasicTest) {
+	std::queue<int> que;
+	BOOST_CHECK(que.empty());
+	que.push(10);
+	que.push(20);
+	BOOST_CHECK_EQUAL(que.size(), 2);
+	BOOST_CHECK_EQUAL(que.front(), 10);
+	BOOST_CHECK_EQUAL(que.back(), 20);
+	que.pop();
+	BOOST_CHECK_EQUAL(que.size(), 1);
+	BOOST_CHECK_EQUAL(que.front(), 20);
+	que.pop();
+	BOOST_CHECK(que.empty());
+}
+BOOST_AUTO_TEST_SUITE_END()
+//优先队列
+#include<queue>
+BOOST_AUTO_TEST_SUITE(PriorityQueueTest)
+BOOST_AUTO_TEST_CASE(PriorityQueueBasicTest) {
+	std::priority_queue<int> pque;//默认是最大堆
+	BOOST_CHECK(pque.empty());
+	pque.push(30);
+	pque.push(10);
+	pque.push(20);
+	BOOST_CHECK_EQUAL(pque.size(), 3);
+	BOOST_CHECK_EQUAL(pque.top(), 30); //最大元素在顶部
+	pque.pop();
+	BOOST_CHECK_EQUAL(pque.size(), 2);
+	BOOST_CHECK_EQUAL(pque.top(), 20);
+	pque.pop();
+	BOOST_CHECK_EQUAL(pque.size(), 1);
+	BOOST_CHECK_EQUAL(pque.top(), 10);
+	pque.pop();
+	BOOST_CHECK(pque.empty());
+	//三个底层元素模板参数
+	std::priority_queue<int, std::vector<int>, std::greater<int>> min_pque; //最小堆（被包装类型，底层容器，比较函数）
+	BOOST_CHECK(min_pque.empty());
+	min_pque.push(30);
+	min_pque.push(10);
+	min_pque.push(20);
+	BOOST_CHECK_EQUAL(min_pque.size(), 3);
+	BOOST_CHECK_EQUAL(min_pque.top(), 10); //最小元素在顶部
+	min_pque.pop();
+	BOOST_CHECK_EQUAL(min_pque.size(), 2);
+	BOOST_CHECK_EQUAL(min_pque.top(), 20); //最小元素在顶部
+}
+BOOST_AUTO_TEST_SUITE_END()
+//6.位级别操作的序列容器：存储固定大小的位序列数据结构
+//作用：节省内存，进行位操作
+#include <bitset>
+BOOST_AUTO_TEST_SUITE(BitsetTest)
+BOOST_AUTO_TEST_CASE(BitsetBasicTest) {
+	//初始化一个8位的位集
+	std::bitset<8> bits1(0b11100011); //默认初始化为0
+	std::bitset<8> bits2("10101011"); //默认初始化为0
+	BOOST_CHECK_EQUAL(bits1.size(), 8);
+	std::bitset<8> bits; //默认初始化为0
+	BOOST_CHECK_EQUAL(bits.size(), 8);
+	BOOST_CHECK_EQUAL(bits.count(), 0); //count()返回设置为1的位数
+	bits.set(2); //将索引2的位设置为1
+	BOOST_CHECK_EQUAL(bits.test(2), true); //test()检查索引2的位是否为1
+	BOOST_CHECK_EQUAL(bits.count(), 1);
+	bits.set(4);
+	BOOST_CHECK_EQUAL(bits.count(), 2);
+	bits.reset(2); //将索引2的位重置为0
+	BOOST_CHECK_EQUAL(bits.test(2), false);
+	BOOST_CHECK_EQUAL(bits.count(), 1);
+	bits.flip(4); //翻转索引4的位
+	BOOST_CHECK_EQUAL(bits.test(4), false);
+	BOOST_CHECK_EQUAL(bits.count(), 0);
+	bits.flip(); //翻转所有位
+	BOOST_CHECK_EQUAL(bits.count(), 8);
+	BOOST_CHECK_EQUAL(bits.to_string(), "11111111"); //将位集转换为字符串表示
+}
+BOOST_AUTO_TEST_SUITE_END()
+//动态位集
+#include <boost/dynamic_bitset.hpp>
+BOOST_AUTO_TEST_SUITE(DynamicBitsetTest)
+BOOST_AUTO_TEST_CASE(DynamicBitsetBasicTest) {
+	boost::dynamic_bitset<> dbits(8); //初始化一个8位的动态位集，默认值为0
+	BOOST_CHECK_EQUAL(dbits.size(), 8);
+	BOOST_CHECK_EQUAL(dbits.count(), 0); //count()返回设置为1的位数
+	dbits.set(3); //将索引3的位设置为1
+	BOOST_CHECK_EQUAL(dbits.test(3), true); //test()检查索引3的位是否为1
+	BOOST_CHECK_EQUAL(dbits.count(), 1);
+	dbits.set(5);
+	BOOST_CHECK_EQUAL(dbits.count(), 2);
+	dbits.reset(3); //将索引3的位重置为0
+	BOOST_CHECK_EQUAL(dbits.test(3), false);
+	BOOST_CHECK_EQUAL(dbits.count(), 1);
+	dbits.flip(5); //翻转索引5的位
+	BOOST_CHECK_EQUAL(dbits.test(5), false);
+	BOOST_CHECK_EQUAL(dbits.count(), 0);
+	dbits.flip(); //翻转所有位
+	BOOST_CHECK_EQUAL(dbits.count(), 8);
+	//BOOST_CHECK_EQUAL(dbits., "11111111"); //将动态位集转换为字符串表示
+}
+BOOST_AUTO_TEST_SUITE_END()
+//特殊的boost容器
+#include <boost/container/small_vector.hpp>
+//small_vector:在栈上分配小容量的向量，当超过该容量时，自动切换到堆分配
+//侵入式容器：容器中的元素包含指向下一个和上一个元素的指针，从而形成链表结构
+//没有连续元素的向量：boost::container::stable_vector
+//支持高效的插入和删除操作，而不影响其他元素的地址
+//具有快速size的单向链表：boost::container::slist
+//多维数组：boost::multi_array：支持多维数组的创建和操作
